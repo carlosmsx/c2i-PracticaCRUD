f7cafbc5bdcf782188b37656305b0daa9f584148
carlosmsx <carlosmsx@gmail.com>
2022-07-05 10:58:43 -0300

    se agrego la clase serie

---
+++ js/serieClass.js
@@ -0,0 +1,12 @@
+export class Serie
+{
+    constructor(codigo, titulo, descripcion, imagen, genero)
+    {
+        this.codigo = codigo
+        this.titulo = titulo
+        this.descripcion = descripcion
+        this.imagen = imagen
+        this.genero = genero
+    }
+    //TODO: agregar los metodos necesarios, getters, setters, etc
+}
